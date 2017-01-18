21f88dcfa1e740d08f7196c008a1428ac8cae71b
leon <chezhe17@gmail.com>
2017-01-18 21:20:37 +0800

    ...

--- .tmp/scripts/main.js
+++ .tmp/scripts/main.js
@@ -6,9 +6,10 @@
  */
 /*global $:false */
 /*global window: false */
-(function() {
-    'use strict';
-    $(function($) {
+(function () {
+    "use strict";
+
+    $(function ($) {
 
         //Detecting viewpot dimension
         var vH = $(window).height();
@@ -19,90 +20,82 @@
         $('.halfwidth').css('width', vW / 2);
         $('.halfheight').css('height', vH / 2);
 
-        
         $('.news-head').setAllToMaxHeight();
         //Mobile Only Navigation (multi level)
-                $('ul.slimmenu').slimmenu({
-                    resizeWidth: '992',
-                    collapserTitle: '',
-                    easingEffect: 'easeInOutQuint',
-                    animSpeed: 'medium',
-                });
+        $('ul.slimmenu').slimmenu({
+            resizeWidth: '992',
+            collapserTitle: '',
+            easingEffect: 'easeInOutQuint',
+            animSpeed: 'medium'
+        });
 
-                $('.slimmenu li a:not(.sub-collapser)').on('click',function(){
-                            $('ul.slimmenu').removeClass('expanded').slideUp();
-                });
-
+        $('.slimmenu li a:not(.sub-collapser)').on('click', function () {
+            $('ul.slimmenu').removeClass('expanded').slideUp();
+        });
 
         //PRELOADER
         $('body, html').addClass('preloader-running');
         $('#mastwrap').css('visibility', 'hidden');
-        $(window).load(function() {
-            $('#status').fadeOut();
-            $('#preloader').delay(1000).fadeOut(1000);
+        $(window).load(function () {
+            $("#status").fadeOut();
+            $("#preloader").delay(1000).fadeOut(1000);
             $('body, html').removeClass('preloader-running');
             $('body, html').addClass('preloader-done');
-            $('#mastwrap').delay(1000).css('visibility',
-                'visible');
+            $("#mastwrap").delay(1000).css('visibility', 'visible');
         });
 
-//Sub Menu Trigger
-        $('.main-menu li a.sub-menu-trigger').on('mouseenter', function(){
+        //Sub Menu Trigger
+        $('.main-menu li a.sub-menu-trigger').on('mouseenter', function () {
             $(this).next('.sub-menu').stop().slideDown(1000);
         });
-        $('.main-menu li').on('mouseleave', function(){
+        $('.main-menu li').on('mouseleave', function () {
             $('.sub-menu').stop().slideUp(1000);
         });
 
-//COMMON UX
-    $('.team-block').on('mouseenter', function(){
-        $(this).find('.team-overlay').slideDown();
-    });
-    $('.team-block').on('mouseleave', function(){
-        $(this).find('.team-overlay').slideUp();
-    });
+        //COMMON UX
+        $('.team-block').on('mouseenter', function () {
+            $(this).find('.team-overlay').slideDown();
+        });
+        $('.team-block').on('mouseleave', function () {
+            $(this).find('.team-overlay').slideUp();
+        });
 
-//PORTFOLIO UX
-        (function( $ ){
-           $.fn.filterPanelTrigger = function() {
-                if($('.works-filter-panel').is(':hidden'))
-                {
+        //PORTFOLIO UX
+        (function ($) {
+            $.fn.filterPanelTrigger = function () {
+                if ($(".works-filter-panel").is(":hidden")) {
                     $('.works-filter-panel').slideDown();
-                    $('.filter-notification a').empty().append( 'Hide Filters' );
-                }
-                else{
+                    $('.filter-notification a').empty().append("Hide Filters");
+                } else {
                     $('.works-filter-panel').slideUp();
-                    $('.filter-notification a').empty().append( 'Show Filters' );
+                    $('.filter-notification a').empty().append("Show Filters");
                 }
-           }; 
-        })( jQuery );
-        $('.filter-notification a').on('click', function(){
+            };
+        })(jQuery);
+        $('.filter-notification a').on('click', function () {
             $().filterPanelTrigger();
         });
         //$('.works-filter-panel').on('mouseleave', function(){
-            //$().filterPanelTrigger();
+        //$().filterPanelTrigger();
         //});
-        $('.works-filter li a').on('click', function(){
+        $('.works-filter li a').on('click', function () {
             $('.works-filter li a').removeClass('filter-active');
             $(this).addClass('filter-active');
             $('html, body').animate({
-                scrollTop: $('#works-container').offset().top-100
+                scrollTop: $("#works-container").offset().top - 100
             }, 1000);
         });
-        
 
-        
-//ISOTOPE
-        
+        //ISOTOPE
+
         //ISOTOPE GLOBALS
         var $container1 = $('.works-container');
 
-
         //ISOTOPE INIT
-        $(window).load(function() {
+        $(window).load(function () {
 
-           //checking if all images are loaded
-            $container1.imagesLoaded( function() {
+            //checking if all images are loaded
+            $container1.imagesLoaded(function () {
 
                 //init isotope once all images are loaded
                 $container1.isotope({
@@ -112,15 +105,13 @@
                     transitionDuration: '0.8s'
                 });
 
-
                 //forcing a perfect masonry layout after initial load
-                setTimeout(function() {
-                $container1.isotope('layout');
+                setTimeout(function () {
+                    $container1.isotope('layout');
                 }, 100);
 
-
                 // triggering filtering
-                $('.works-filter li a').on('click', function() {
+                $('.works-filter li a').on('click', function () {
                     $('.works-filter li a').removeClass('active');
                     $(this).addClass('active');
 
@@ -128,59 +119,51 @@
                     $('.works-container').isotope({
                         filter: selector
                     });
-                    setTimeout(function() {
+                    setTimeout(function () {
                         $container1.isotope('layout');
                     }, 700);
                     return false;
                 });
 
-
                 //Isotope ReLayout on Window Resize event.
-                $(window).on('resize', function() {
+                $(window).on('resize', function () {
                     $container1.isotope('layout');
                 });
 
                 //Isotope ReLayout on device orientation changes
-                window.addEventListener('orientationchange', function() {
+                window.addEventListener("orientationchange", function () {
                     $container1.isotope('layout');
                 }, false);
-
             });
-
         });
 
+        //VENOBOX
+        $('.venobox, .image-lightbox-link').venobox({
+            numeratio: true,
+            infinigall: true
+        });
 
+        //CAROUSEL
+        $(".project-carousel").owlCarousel({
+            loop: true,
+            margin: 0,
+            dots: false,
+            nav: true,
+            navText: false,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 1
+                },
+                1000: {
+                    items: 1
+                }
+            }
+        });
 
-
-//VENOBOX
-    $('.venobox, .image-lightbox-link').venobox({
-        numeratio: true,
-        infinigall: true
-    });   
-        
-
-//CAROUSEL
- $('.project-carousel').owlCarousel({
-                    loop:true,
-                    margin:0,
-                    dots:false,
-                    nav:true,
-                    navText: false,
-                    responsive:{
-                        0:{
-                            items:1
-                        },
-                        600:{
-                            items:1
-                        },
-                        1000:{
-                            items:1
-                        }
-                    }
-                });
-
-
- //PARALLAX
+        //PARALLAX
         //Initialize Each Parallax Layer  
         function parallaxInit() {
             $.stellar({
@@ -191,19 +174,16 @@
         if (!device.tablet() && !device.mobile()) {
 
             //Activating Parallax effect if non-mobile device is detected
-            $(window).bind('load', function() {
+            $(window).bind('load', function () {
                 parallaxInit();
             });
-
-
         } else {
 
             //Dectivate Parallax effect if mobile device is detected (bg image is displayed)
             $('.parallax, .parallax-layer').addClass('no-parallax');
-
-        }   
-
+        }
     });
     // $(function ($)  : ends
 })();
 //  JSHint wrapper $(function ($)  : ends
+//# sourceMappingURL=main.js.map
