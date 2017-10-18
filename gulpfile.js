7152188b6c42075662423a16559c44b315e2b472
Yan Zhu <picturepan2@hotmail.com>
2017-10-18 22:36:59 +0800

    增加艺术家 ZHI16

--- gulpfile.js
+++ gulpfile.js
@@ -162,7 +162,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
